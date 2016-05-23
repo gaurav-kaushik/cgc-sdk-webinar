a05432c5706a80e7d78475dd4b213c4f9e342d05
Gaurav Kaushik <gaurav.kaushik@sbgenomics.com>
2016-05-23 13:53:26 -0400

    Added CWL JSON that can be imported to the CGC

--- GeneExpressionWF/DifferentialExpression/diff.R
+++ GeneExpressionWF/DifferentialExpression/diff.R
@@ -6,28 +6,29 @@ library(RColorBrewer)
 options(warn=-1)
 print("Library imports done")
 
-# Collect arguments from the command line
-args <- commandArgs(TRUE)
-
 # Read CSV files (harcoded for now)
-gene <- read.csv(args[1], header=TRUE, row.names=1) # get the counts per gene (row) per sample/case (col)
-meta <- read.csv(args[2], header=TRUE) # get the metadata matrix
-print("CSV files read")
+# Collect arguments from the command line
+# args <- commandArgs(TRUE)
+# gene <- read.csv(args[1], header=TRUE, row.names=1) # get the counts per gene (row) per sample/case (col)
+# meta <- read.csv(args[2], header=TRUE) # get the metadata matrix
 
 ## For testing only:
-# gene = read.csv("brca_gene.csv", header = TRUE, row.names=1)
-# meta = read.csv("brca_meta.csv", header = TRUE)
-#gene = gene[,1:20]
-#meta = meta[1:20,]
+gene = read.csv("PanCan_by_gene.csv", header = TRUE, row.names=1)
+meta = read.csv("PanCan_metadata.csv", header = TRUE)
+# gene = gene[,1:200]
+# meta = meta[1:200,]
+
+print("CSV files read")
 
 # Get the Differential Expression results
 print("Initializing Differential Expression Analysis -- go grab a coffee :)")
 meta.df = data.frame(meta)
-dds = DESeqDataSetFromMatrix(countData = gene, colData = meta.df, ~ sample_type + X.case_id)
-design(dds) <- ~ sample_type + X.case_id
+dds = DESeqDataSetFromMatrix(countData = gene, colData = meta.df, ~ sample_type + X.case_id + investigation)
+design(dds) <- ~ sample_type + X.case_id + investigation
 dds = DESeq(dds)
 #resultsNames(dds)
 res <- results(dds, contrast = c("sample_type", "Solid Tissue Normal", "Primary Tumor"))
+resInv <- results(dds, contrast = c("investigation", "TCGA-LUAD","TCGA-LUSC"))
 resOrdered <- res[order(res$padj),] # order the results by adjusted p-value
 print("Differential Expression analysis DONE!")
 
@@ -46,27 +47,35 @@ pdf(plot_title)
 # Plot -- Volcano plot
 plotMA(res,main="DESeq2", ylim = c(-4,4))
 
-# Heatmap -- Gene Expression
-select <- order(rowMeans(counts(dds,normalized=TRUE)), decreasing=TRUE)[1:20]
+# Heatmap -- Gene Expression by Sample Type
+select <- order(rowMeans(counts(dds,normalized=TRUE)), decreasing=TRUE)[1:200]
 nt <- normTransform(dds) # log2(count+1) normalization 
 log2.norm.counts <- assay(nt)[select,]
 df <- as.data.frame(colData(dds)[,c("sample_type")])
 colnames(df) <- "sample_type"
 pheatmap(log2.norm.counts, cluster_rows=FALSE, show_rownames=FALSE, cluster_cols=FALSE, annotation_col=df)
 
+# Heatmap -- Gene Expression by Investigation
+select <- order(rowMeans(counts(dds,normalized=TRUE)), decreasing=TRUE)[1:200]
+nt <- normTransform(dds) # log2(count+1) normalization 
+log2.norm.counts <- assay(nt)[select,]
+df <- as.data.frame(colData(dds)[,c("investigation")])
+colnames(df) <- "investigation"
+pheatmap(log2.norm.counts, cluster_rows=FALSE, show_rownames=FALSE, cluster_cols=FALSE, annotation_col=df)
+
 # Heatmap -- Sample-Sample Distance 
 #rld <- rlog(dds,blind=FALSE)
 vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
-rld <- vsd # for now, to swap out rld
-sampleDists <- (dist(t(assay(rld))))
+sampleDists <- (dist(t(assay(vsd))))
 sampleDistMatrix <- as.matrix(sampleDists)
 colnames(sampleDistMatrix) <- NULL
 colors <- colorRampPalette( rev(brewer.pal(9,"Blues")))(255)
-rownames(sampleDistMatrix) <- paste(rld$condition, rld$sample_type, sep="-")
+rownames(sampleDistMatrix) <- paste(vsd$condition, vsd$sample_type, sep="-")
 pheatmap(sampleDistMatrix, clustering_distance_rows=sampleDists, clustering_distance_cols=sampleDists,col=colors)
 
 # PCA plot -- sample-type
-plotPCA(rld,intgroup=c("sample_type"))
+plotPCA(vsd,intgroup=c("sample_type"))
+plotPCA(vsd,intgroup=c("investigation"))
 
 # Finish up
 dev.off()
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-#Cancer Genomics Cloud Webinar -- Deploying your own tools
+#Seven Bridges Cancer Genomics Cloud -- Deploying your own tools
 
+## Overview
 ####This project contains these tools and workflows:
-- grep (tool)
+- sonnet-grep (tool)
 - dna2protein (workflow)
 	- transcribe (tool)
 	- translate (tool)
@@ -9,11 +10,11 @@
 	- Gene Expression Munger (tool)
 	- Differential Expression (tool)
 
-##sonnet-grep
+##grep
 This tool looks for patterns in a text file and outputs the lines that match. *grep* is a standard Linux command line tool.
 
 ##dna2protein
-This workflow takes a text file with a DNA sequence and outputs a text file with a peptide sequence, if possible. *dna2protein* is a custom workflow written in Python that is composed of *transcribe.py* (dna2rna) and *translate.py* (rna2protein).
+This workflow takes a input text file with a DNA sequence and outputs a text file with a peptide sequence from the first ORF, if possible. *dna2protein* is a custom workflow written in Python that is composed of *transcribe.py* (DNA -> mRNA) and *translate.py* (mRNA -> protein).
 
 ##GeneExpressionWF
-This workflow 1) merges any number of Level 3 TCGA RNASeq Gene Expression Quantification files into a single matrix and also produces a "metadata matrix" with information about individual cases (*GeneExpressionMunger*) using Python and 2) runs a custom differential expression analysis that produces a matrix of (adj) p-values per gene based on differences among sample-types (e.g. Solid Tissue Normal and Primary Tumor) as well as some plots in PDF (*DifferentialExpression)*. 
+This workflow produces a differential expression analysis from an array of Level 3 TCGA RNASeq Gene Expression Quantification files. It first merges the files into a single matrix and produces a "metadata matrix"  from the metadata (e.g. sample type, gender) from each individual case (*GeneExpressionMunger*, *munger.py*). It then runs a custom  differential expression analysis that produces a matrix of (adj) p-values per gene based on differences among sample-types (e.g. Solid Tissue Normal and Primary Tumor) as well as some plots in PDF (*DifferentialExpression*, *diff.R*). 
